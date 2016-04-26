916699a95b77efcafcf799fb17d31fbebde04e3a
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2016-04-26 15:27:28 +0200

    Add move line up and down functions.

--- custom/functions.el
+++ custom/functions.el
@@ -12,6 +12,20 @@
   (interactive)
   (insert "¯\\_(ツ)_/¯"))
 
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
 (defun swap-windows ()
   "Swap your windows."
   (interactive)

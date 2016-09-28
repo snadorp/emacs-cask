c74b6ce91cf79d291777ac6df3d23ce1fc13d77d
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2016-09-28 15:41:56 +0200

    Add bindings for multiline mode.

--- custom/key-bindings.el
+++ custom/key-bindings.el
@@ -63,3 +63,11 @@
 ;; Fix side scrolling
 (global-set-key [wheel-right] 'scroll-left)
 (global-set-key [wheel-left] 'scroll-right)
+
+;; multicursor
+;;; When you have an active region that spans multiple lines, the following will add a cursor to each line:
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+;;; When you want to add multiple cursors not based on continuous lines, but based on keywords in the buffer, use:
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
